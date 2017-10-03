6ee7b1f4fac4eef9ac9024a3416c5a3278d2c10a
Borhan Chowdhury <borhan.chittagong@gmail.com>
2017-10-03 19:32:20 +0600

    Auth class improved

---
+++ app/src/main/java/org/badhan/blooddonor/service/profile/UserDetailUpdated.java
@@ -0,0 +1,11 @@
+package org.badhan.blooddonor.service.profile;
+
+import org.badhan.blooddonor.entity.User;
+
+public class UserDetailUpdated {
+    public User user;
+
+    public UserDetailUpdated(User user){
+        this.user = user;
+    }
+}
